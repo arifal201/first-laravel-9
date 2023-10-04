76058e678e8c0638082c16cb6f4d37b50539c3d1
arifal <43651600+arifal201@users.noreply.github.com>
2023-10-04 18:03:53 +0700

    Change: products index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,13 +1,16 @@
-<h1>Hello index products</h1>
+<h1>products</h1>
 <a href="{{ $exportExcel }}">Download Excel</a>
 <a href="{{ $exportPdf }}">Download Pdf</a>
 <table>
   <thead>
     <tr>
-      <th scope="col">id</th>
-      <th scope="col">name</th>
-      <th scope="col">price</th>
-      <th scope="col">description</th>
+      <th scope="col">Id</th>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Price</th>
+      <th scope="col">Year</th>
+      <th scope="col">Category</th>
+      <th scope="col">Stock</th>
       <th scope="col">created_at</th>
       <th scope="col">updated_at</th>
     </tr>
@@ -17,8 +20,11 @@
       <tr>
         <td>{{ $product->id }}</td>
         <td>{{ $product->name }}</td>
-        <td>{{ $product->price }}</td>
         <td>{{ $product->description }}</td>
+        <td>{{ $product->price }}</td>
+        <td>{{ $product->year }}</td>
+        <td>{{ $product->category }}</td>
+        <td>{{ $product->stock }}</td>
         <td>{{ $product->created_at }}</td>
         <td>{{ $product->updated_at }}</td>
       </tr>
